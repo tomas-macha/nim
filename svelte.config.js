59d62e21827ffa402c71a0cb46530fe44dbddbbb
Tomáš Mácha <112073439+tomas-macha@users.noreply.github.com>
2024-08-20 22:28:14 +0200

    SSG settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-//import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
-
-export default config;
