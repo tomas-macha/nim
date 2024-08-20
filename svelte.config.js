fad2f87da5ce92d1845a3e88b05e55a52640c7a6
Tomáš Mácha <112073439+tomas-macha@users.noreply.github.com>
2024-08-20 22:44:42 +0200

    Cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,11 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
