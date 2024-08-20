0daa237e91c12fb7b45a511637713ada5f4b49d8
Tomáš Mácha <112073439+tomas-macha@users.noreply.github.com>
2024-08-20 22:30:15 +0200

    Build fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
+//import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
@@ -11,3 +17,5 @@ export default {
 		})
 	}
 };
+
+export default config;
